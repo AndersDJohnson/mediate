6c892980c953545d7a9cd7816b0e31344afd6b8c
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-04-05 19:11:18 -0500

    update backend

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ var app = express();
 var api = require('./lib/api');
 var ui = require('./lib/ui');
 
-app.use('/api', api);
+app.use(express.logger('short'));
+
+var apiUri = '/api';
+app.use(apiUri, api({
+  uri: apiUri
+}));
+
 app.use('/', ui);
 
 app.listen(9000);
